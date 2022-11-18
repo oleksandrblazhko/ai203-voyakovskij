50b29a7cc992490999d42372576ffb528fc250c9
rumplestiltsk1n <79450629+rumplestiltsk1n@users.noreply.github.com>
2022-11-19 01:33:16 +0200

    Add files via upload

---
+++ 3-SoftwareConstruction/3.3-ContinuousIntegration/ProgramCode.js
@@ -0,0 +1,17 @@
+function dateValidation () {
+  // Create Date object
+  let tomorrowDate = new Date() // current date
+  tomorrowDate.setDate(tomorrowDate.getDate() + 1) // add 1 day to current date
+
+  // Fix missing a day
+  const offset = tomorrowDate.getTimezoneOffset()
+  tomorrowDate = new Date(tomorrowDate.getTime() - (offset * 60 * 1000))
+
+  // date input field
+  const datePicker = document.getElementById('datePicker')
+
+  datePicker.valueAsDate = tomorrowDate // default date
+  datePicker.setAttribute('min', tomorrowDate.toISOString().substring(0, 10)) // min value is tomorrow date
+}
+
+dateValidation()
